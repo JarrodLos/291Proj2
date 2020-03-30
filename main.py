53520224c1332cc8cec2bf96860529c89813ae08
Jarrod Los <56139570+JarrodLos@users.noreply.github.com>
2020-03-29 22:15:44 -0600

    added comments and skeleton code for part 2

--- main.py
+++ main.py
@@ -64,6 +64,37 @@ def customIn(prompt = ""):
     else:
         return myInput
 
+# Checks the query for various input types, wild cards and conditions
+def checkQuery(query):
+    ##### Some notes from building the queries in PT 2 as well as possible breakdown ######
+    # All matches are case-insensitive, query has been passed in casted to "lowercase"
+    # There is one or more spaces between the conditions.
+        # - Parse by spaces, and iterate through to see if it contains anything below?
+
+    # Dates are formatted as yyyy/mm/dd in queries but they are stored as timestamps in the data file
+        # Must be converted to timestamp before a search can be performed
+        # I'll implement this from my code from project 1
+
+    # You can assume every query has at least one condition on an indexed column,
+    # meaning the conditions on price and date can only be used if a condition on review/product terms or review scores is also present.
+
+    # if query contains ":"
+        # Parse and first half is the db, second half is the key
+
+    # if query contains ":" and " "
+        # Parse and first half is the db, remaining terms are key and cond.
+
+    # if query contains "%" (Wildcard only on end of query)
+        # Query for partial match
+
+    # if query contains >, < or = and # of " " = 2
+        # Ranged query (Price, Date, Score)
+
+    # if query contains >, < or = and # of " " > 2
+        # Ranged query + conditions (Price, Date, Score)
+
+    return True # Change this, it was giving bugs with no return
+
 # Handles queries and navigation of user input for Part 2
 def queryListener():
     print("\nPlease enter a query or change mode, for more detail enter '?'")
@@ -72,11 +103,17 @@ def queryListener():
     if (query == "modeChange"): # Mode has been changed b/t brief or full
         return True
 
-    elif (query == "?"):
+    elif (query == "?"): # Provides more detail on options for Part 2
         print("\nTo change mode: enter 'output=brief' or 'output=full' for a brief or more detailed output")
         print("To run a query: simply enter your query!")
         print("To exit the program, enter 'exit' at any time")
+        return True
 
+    else: # Check & run the query!
+        checkQuery(query.lower()) # Return checkQuery and make it return T or F depending on state?
+        return True
+
+    # Eventually change this, but for now just loop through each time
     return True
 
 if (__name__ == "__main__"):
